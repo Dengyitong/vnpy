bbdb7efbfe1c92220318ef817fb8d6a62d0266dd
Dengyitong <727379993@qq.com>
2018-05-17 23:01:39 +0800

    删除多余的文件，留下example

--- beta/api/coincheck/__init__.py
+++
@@ -1,3 +0,0 @@
-# encoding: UTF-8
-
-from vncoincheck import TradeApi, DataApi
--- beta/api/korbit/__init__.py
+++
@@ -1,3 +0,0 @@
-# encoding: UTF-8
-
-from vnkorbit import Korbit_TradeApi, Korbit_DataApi , KORBIT_ALL_SYMBOL_PAIR , KORBIT_ALL_SYMBOLS
